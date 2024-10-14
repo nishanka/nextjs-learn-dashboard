f71ce4b6172b12e97cff8aabef4417629804c440
nishanka <nishanka.rupasinghe@gmail.com>
2024-10-15 01:29:37 +1000

    Add Files

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p>Hey! Next JS Dev, you are in Customers Page</p>;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p>Hey! Next JS Developer, Here is the Dashboard Page. Congratulations!</p>;
+}
